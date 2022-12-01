4485b56b113e21f494242d11629e201021d8aa76
talha <talhaamir2014@gmail.com>
2022-12-01 16:14:54 +0500

    fix default

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    #[Route('/', name: 'app_default')]
     public function index(): Response
     {
         $user = $this->getUser();
